64f5c11a215ef81c27fff95f4f6a0aededf75971
Ivan <21key47@mybhs.org>
2017-10-03 16:40:18 -0700

    Added SmartDashboard
    to Drive with joystick

--- src/org/usfirst/frc/team5026/robot/commands/DriveWithJoystickCommand.java
+++ src/org/usfirst/frc/team5026/robot/commands/DriveWithJoystickCommand.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team5026.robot.Robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -23,6 +24,8 @@ public class DriveWithJoystickCommand extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.drive.robotDrive.arcadeDrive(Robot.oi.stick);
+    	SmartDashboard.putNumber("JoystickX", Robot.oi.stick.getX());
+       	SmartDashboard.putNumber("JoystickY", Robot.oi.stick.getY());
     }
 
     // Make this return true when this Command no longer needs to run execute()
